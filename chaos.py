35e2ec0a6bd30a7233f90eb32e1a0fa3fe007aeb
Skylar Ulrich <Skylar.ulrich1@marist.edu>
2021-02-22 12:20:36 -0500

    chap1 exercise 6a

--- chaos.py
+++ chaos.py
@@ -4,8 +4,7 @@
 def main():
     print("This program illustrates a chaotic function")
     x= eval(input("Enter a number between 0 and 1: "))
-    n = eval(input("How many numbers should I print?"))
-    for i in range(n):
+    for i in range(100):
         x= 3.9 * x * (1-x)
         print(x)
 
