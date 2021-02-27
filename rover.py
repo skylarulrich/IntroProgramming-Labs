bbb6d0b76a3219f22c5ab2bfdaac6867a369904a
Skylar Ulrich <Skylar.ulrich1@marist.edu>
2021-02-26 22:19:18 -0500

    Solution to Curiosity rover exercise.

---
+++ rover.py
@@ -0,0 +1,8 @@
+#code to calculate how long Curiosity to reach NASA when Mars is at its closest orbit
+
+def main():
+    howlong = 34000000/186000
+    print("It takes", howlong, "seconds for a picture from Curiosity to reach NASA when Mars is at its closeset to Earth")
+
+
+main()
